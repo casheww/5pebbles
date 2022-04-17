5b42b86445eaa430b98d825092be4adeb1ef2b95
casheww <51793722+casheww@users.noreply.github.com>
2022-04-17 14:24:56 +0100

    fix systemctl script

--- setup.sh
+++ setup.sh
@@ -5,10 +5,12 @@ python3 db_setup.py
 echo "python packages installed and database setup done"
 
 sudo cp rw_wiki_bot.service /etc/systemd/system/
+echo "service file copied"
 sudo systemctl daemon-reload
+echo "systemctl daemon reloaded"
 sudo systemctl enable rw_wiki_bot
-sudo service rw_wiki_bot start
-
+echo "systemctl service enabled"
+sudo systemctl start rw_wiki_bot start
 echo "systemctl setup done"
 
 echo "reminder: I need a _keys.json file with a version string and Discord token"
